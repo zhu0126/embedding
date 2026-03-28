971ee907a628f360a6e075c3c348bfb84dcb96c9
zhu0126 <a0903223681@gmail.com>
2026-03-28 17:28:34 +0800

    Update server.py

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 model = SentenceTransformer('ZHU1107/my-embedding-model',use_auth_token=hf_token)
 # model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
 
+@app.route("/health", methods=["GET"])
+def health():
+    return "ok", 200
+
 @app.route("/embedding", methods=["POST"])
 def get_embedding():
     try:
