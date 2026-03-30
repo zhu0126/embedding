484029a9aa8032f616c63f1b325620c66da619db
zhu0126 <a0903223681@gmail.com>
2026-03-30 15:16:48 +0800

    Update server.py

--- server.py
+++ server.py
@@ -51,4 +51,4 @@ def get_embedding():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000,threaded=True)
+    app.run(host="0.0.0.0",port=$PORT,threaded=True)
