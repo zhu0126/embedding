056651363397a41175d8deb36d902e922e65e80a
zhu0126 <a0903223681@gmail.com>
2026-03-30 15:34:26 +0800

    Update server.py

--- server.py
+++ server.py
@@ -50,6 +50,3 @@ def get_embedding():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0",port=port,threaded=True)
